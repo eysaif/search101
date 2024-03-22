15de79a2a7c634ba724bffc94c742fb671511e32
shaikh Saifuddin <u4saif@gmail.com>
2024-03-22 13:59:42 +0530

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   isCollapsed = false;
+  currentActiveResouce = { label: '1337x', key: '1337x' }; 
+  resoureceList = [
+    { label: '1337x', key: '1337x' },
+    { label: 'Yts', key: 'yts' },
+    { label: 'Eztv', key: 'eztv' },
+    { label: 'Torrent Galaxy', key: 'tgx' },
+    { label: 'Torlock', key: 'torlock' },
+    { label: 'PirateBay', key: 'piratebay' },
+    { label: 'Nyaasi', key: 'nyaasi' },
+    { label: 'Rarbg', key: 'rarbg' },
+    { label: 'Ettv', key: 'ettv' },
+    { label: 'Zooqle', key: 'zooqle' },
+    { label: 'KickAss', key: 'kickass' },
+    { label: 'Bitsearch', key: 'bitsearch' },
+    { label: 'Glodls', key: 'glodls' },
+    { label: 'MagnetDL', key: 'magnetdl' },
+    { label: 'LimeTorrent', key: 'limetorrent' },
+    { label: 'TorrentFunk', key: 'torrentfunk' },
+    { label: 'TorrentProject', key: 'torrentproject' },
+  ];
+
+  onClickHandler(newValue:any){
+    this.currentActiveResouce= newValue;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchbarComponent } from './component/searchbar/searchbar.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -14,13 +13,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +29,8 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     IconsProviderModule,
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    NzInputModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
